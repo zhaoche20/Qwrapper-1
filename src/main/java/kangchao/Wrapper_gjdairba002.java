ae3110ca006f4134893dacc11a7a69f1722fd0da
chao.kang <chao.kang@QF-kangchao.qunarservers.com>
2014-07-03 20:09:06 +0800

    airfb001 修改

--- src/main/java/kangchao/Wrapper_gjdairba002.java
+++ src/main/java/kangchao/Wrapper_gjdairba002.java
@@ -2,7 +2,6 @@ package kangchao;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
-import org.apache.commons.httpclient.protocol.Protocol;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
